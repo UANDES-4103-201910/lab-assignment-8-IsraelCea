c3595c92ba38dbd4aeadc976da37399de0aefcd4
IsraelCea <ivcea@miuandes.cl>
2019-05-09 19:44:32 -0400

    HideShow working

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.5.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.6'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -63,3 +63,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Add jquery
 gem 'jquery-rails'
+
--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,66 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
+/*
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+};
+*/
+$(document).ready(function() {
+
+        var keyBoardContainer = document.getElementsByName("kcontainer");
+
+        function hideKeyboard() {
+            /*keyBoardContainer.toggle; //ESTO NO FUNCIONA*/
+            $('#kcontainer').toggle();//ESTO FUNCIONA BIEN
+        }
+        //Se define el boton que esconde el teclado y su respectivo evento
+        var btnHideShow = document.getElementById("btnEsconder");
+        btnHideShow.onclick = hideKeyboard;
+
+        //Se define el cuadro de texto
+        var cuadroTexto = document.getElementById("cTexto");
+
+
+        function fnEscribir() {
+            $('#cTexto').append(tecla.text());
+
+            /*var tecla = event.target;
+            var textoTecla =  tecla.text();
+            cuadroTexto.append(textoTecla);*/
+        }
+
+
+        var tecla = document.getElementsByClassName("key");
+        tecla.onclick = fnEscribir;
+
+
+})
+
+
+/*
+_________________________________________________________________________________________________________
+ESTE CODIGO FUNCIONA PARA BOTON CLOSE/HIDE Y ENVÌA EL VALOR DE LA TECLA A LA CONSOLA
+
+
+
+$(document).ready(function(){
+
+    console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+    $("#btnEsconder").click(function(){
+        $('#kcontainer').toggle();
+    });
+
+//esta funcion envìa el valor de la tecla a la consola
+
+    $(".key").click(function(){
+        console.log($(this).text());
+    });
+
+
 });
+
+____________________________________________________________________________________________________________
+
+*/
+
