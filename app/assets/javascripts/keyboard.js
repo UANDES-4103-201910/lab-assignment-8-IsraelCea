353b72120825459f5ce0b14ba17b5ef82ec44ded
IsraelCea <ivcea@miuandes.cl>
2019-05-09 23:31:14 -0400

    writes in text area

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -4,7 +4,35 @@ $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 
 };
+
+_________________________________________________________________________________________________________
+ESTE CODIGO FUNCIONA PARA BOTON CLOSE/HIDE Y ENVÌA EL VALOR DE LA TECLA A LA CONSOLA
+
+
+
+$(document).ready(function(){
+
+    console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+    $("#btnEsconder").click(function(){
+        $('#kcontainer').toggle();
+    });
+
+//esta funcion envia el valor de la tecla a la consola
+
+    $(".key").click(function(){
+        console.log($(this).text());
+    });
+
+
+});
+
+____________________________________________________________________________________________________________
+
 */
+
+
+
 $(document).ready(function() {
 
         var keyBoardContainer = document.getElementsByName("kcontainer");
@@ -33,34 +61,22 @@ $(document).ready(function() {
         var tecla = document.getElementsByClassName("key");
         tecla.onclick = fnEscribir;
 
+        //esta funcion envia el valor de la tecla a la consola
+        /*$(".key").click(function(){
+            console.log($(this).text());
+        });*/
 
-})
+        //Esta funcion escribe el valor del texto en el cuadro de escritura
+        $(".key").click(function(){
 
-
-/*
-_________________________________________________________________________________________________________
-ESTE CODIGO FUNCIONA PARA BOTON CLOSE/HIDE Y ENVÌA EL VALOR DE LA TECLA A LA CONSOLA
-
-
-
-$(document).ready(function(){
-
-    console.log('Document ready! [app/assets/javascripts/keyboard.js]');
-
-    $("#btnEsconder").click(function(){
-        $('#kcontainer').toggle();
-    });
-
-//esta funcion envìa el valor de la tecla a la consola
-
-    $(".key").click(function(){
-        console.log($(this).text());
-    });
+            $('#elTexto').append($(this).text());}
+        )
 
 
 });
 
-____________________________________________________________________________________________________________
+
+/*
 
 */
 
